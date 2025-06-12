2e8a6713336e74a04b9b389f6f6a036d840975b8
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2025-06-12 21:27:39 +0300

    dev: Get the IDs also

--- Api/Models/GetLogResponseObject.cs
+++ Api/Models/GetLogResponseObject.cs
@@ -2,6 +2,8 @@ namespace PersonalLogManager.Api.Models
 {
     public class GetLogResponseObject
     {
+        public string Id { get; set; }
+
         public string Date { get; set; }
 
         public string Time { get; set; }
--- DataAccess/DataObjects/PersonalLogEntity.cs
+++ DataAccess/DataObjects/PersonalLogEntity.cs
@@ -1,3 +1,4 @@
+using System;
 using NuciDAL.DataObjects;
 
 namespace PersonalLogManager.DataAccess.DataObjects
