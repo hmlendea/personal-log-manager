9e1b6f2eee95d23d65f3844f904e4296ac2f9989
hmlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2025-10-24 13:49:46 +0300

    security: Implemented NuciAPI and NuciSecurity

--- Api/Models/GetLogRequest.cs
+++ Api/Models/GetLogRequest.cs
@@ -1,18 +1,25 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using NuciAPI.Requests;
+using NuciSecurity.HMAC;
 
 namespace PersonalLogManager.Api.Models
 {
-    public class GetLogRequest
+    public class GetLogRequest : NuciApiRequest
     {
+        [HmacOrder(1)]
         public string Date { get; set; }
 
+        [HmacOrder(2)]
         public string Time { get; set; }
 
+        [HmacOrder(3)]
         public string Template { get; set; }
 
+        [HmacOrder(4)]
         public Dictionary<string, string> Data { get; set; }
 
+        [HmacOrder(5)]
         [Range(1, 10000)]
         public int Count { get; set; } = 1;
     }
--- Api/Models/StoreLogRequest.cs
+++ Api/Models/StoreLogRequest.cs
@@ -1,19 +1,26 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using NuciAPI.Requests;
+using NuciSecurity.HMAC;
 
 namespace PersonalLogManager.Api.Models
 {
-    public class StoreLogRequest
+    public class StoreLogRequest : NuciApiRequest
     {
         [Required]
+        [HmacOrder(1)]
         public string Date { get; set; }
 
+        [HmacOrder(2)]
         public string Time { get; set; }
 
+        [HmacOrder(3)]
         public string TimeZone { get; set; }
 
+        [HmacOrder(4)]
         public string Template { get; set; }
 
+        [HmacOrder(5)]
         public Dictionary<string, string> Data { get; set; }
     }
 }
