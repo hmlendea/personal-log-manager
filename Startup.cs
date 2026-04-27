8378479d837698645ef4dfed3abf7511f91eed60
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-04-27 08:19:42 +0300

    deps: Updated the dependenies

--- Startup.cs
+++ Startup.cs
@@ -5,7 +5,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using NuciAPI.Middleware;
+using NuciAPI.Middleware.ExceptionHandling;
+using NuciAPI.Middleware.Logging;
+using NuciAPI.Middleware.Security;
 using PersonalLogManager.Configuration;
 
 namespace PersonalLogManager
